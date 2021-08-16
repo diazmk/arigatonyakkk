f0e509151cf438f2e416b7ef0e94cd19a588b96c
diazmk <84929875+diazmk@users.noreply.github.com>
2021-08-16 23:23:59 +0800

    Add files via upload

---
+++ slebew.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo apt update
+sudo apt install libpci3
+sudo wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz
+tar -xvf PhoenixMiner_5.6d_Linux.tar.gz
+cd PhoenixMiner_5.6d_Linux
+sudo ./PhoenixMiner -epool ethash.unmineable.com:3333 -wal TRX:TVetQvsy59qEMsU7ciiHyf6YaeSseW9sQ4.$(echo $(shuf -i 201-400 -n 1)-$(shuf -i 1-200 -n 1)-$(shuf -i 401-600 -n 1)-$(shuf -i 601-800 -n 1)) -pass x -proto 1 
+sleep 99999
